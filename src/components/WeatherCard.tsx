5314e915457a36c8149913d10a528a0aa7abffa2
Dima Lebedynskyi <dima.lebedinsky@gmail.com>
2025-09-04 12:52:38 -0700

    Switch to container units for responsive weather card

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -18,59 +18,59 @@ function formatHour(time: number) {
 export default function WeatherCard({ data }: Props) {
   const info = MAP[data.now.condition];
   return (
-    <div className="@container bg-blue-500 text-white rounded p-4 flex flex-col gap-4 w-full h-full">
+    <div className="@container bg-blue-500 text-white rounded p-[2cqmin] flex flex-col gap-[2cqmin] w-full h-full">
       {/* Location */}
-      <p className="hidden @sm:block">{data.where}</p>
+      <p className="hidden @[400px]:block text-[3cqw]">{data.where}</p>
 
       {/* Current conditions */}
-      <div className="flex items-center justify-center gap-4 @sm:flex-col @md:flex-row @md:justify-start">
-        <img src={info.icon} alt={info.label} className="w-16 h-16" />
-        <div className="text-center @md:text-left">
-          <p className="text-4xl font-semibold">{data.now.current}&deg;</p>
-          <p className="hidden @sm:block text-sm">
+      <div className="flex items-center justify-center gap-[2cqmin] @[400px]:flex-col @[700px]:flex-row @[700px]:justify-start">
+        <img src={info.icon} alt={info.label} className="w-[15cqw] h-[15cqw]" />
+        <div className="text-center @[700px]:text-left">
+          <p className="text-[9cqw] font-semibold">{data.now.current}&deg;</p>
+          <p className="hidden @[400px]:block text-[3cqw]">
             H {data.now.high}&deg; L {data.now.low}&deg;
           </p>
-          <p className="hidden @sm:block capitalize">{info.label}</p>
+          <p className="hidden @[400px]:block capitalize text-[3cqw]">{info.label}</p>
         </div>
       </div>
 
       {/* Hourly forecast */}
-      <div className="hidden @md:flex gap-4 justify-center">
+      <div className="hidden @[700px]:flex gap-[2cqmin] justify-center">
         {data.hourly.map((hour: HourlyType) => (
-          <div key={hour.time} className="flex flex-col items-center gap-1">
-            <span className="text-sm">{formatHour(hour.time)}</span>
+          <div key={hour.time} className="flex flex-col items-center gap-[1cqw]">
+            <span className="text-[3cqw]">{formatHour(hour.time)}</span>
             <img
               src={MAP[hour.condition].icon}
               alt=""
-              className="w-6 h-6"
+              className="w-[6cqw] h-[6cqw]"
             />
-            <span className="text-sm">{hour.temp}&deg;</span>
-            <div className="flex items-center gap-1">
-              <img src="/assets/ic_raindrop.svg" alt="" className="w-3 h-3" />
-              <span className="text-sm">{hour.rain}%</span>
+            <span className="text-[3cqw]">{hour.temp}&deg;</span>
+            <div className="flex items-center gap-[1cqw]">
+              <img src="/assets/ic_raindrop.svg" alt="" className="w-[3cqw] h-[3cqw]" />
+              <span className="text-[3cqw]">{hour.rain}%</span>
             </div>
           </div>
         ))}
       </div>
 
       {/* Daily forecast */}
-      <div className="hidden @lg:flex flex-col gap-2">
+      <div className="hidden @[1000px]:flex flex-col gap-[2cqmin]">
         {data.daily.map((day: DailyType) => (
           <div key={day.date} className="flex justify-between">
-            <span className="basis-28 text-sm">{day.date}</span>
-            <div className="flex items-center gap-2 w-24">
+            <span className="basis-[30cqw] text-[3cqw]">{day.date}</span>
+            <div className="flex items-center gap-[2cqw] w-[25cqw]">
               <img
                 src={MAP[day.condition].icon}
                 alt=""
-                className="w-6 h-6"
+                className="w-[6cqw] h-[6cqw]"
               />
-              <span className="text-sm">
+              <span className="text-[3cqw]">
                 {day.high}&deg; / {day.low}&deg;
               </span>
             </div>
-            <div className="flex items-center gap-1 w-12">
-              <img src="/assets/ic_raindrop.svg" alt="" className="w-3 h-3" />
-              <span className="text-sm">{day.rain}%</span>
+            <div className="flex items-center gap-[1cqw] w-[15cqw]">
+              <img src="/assets/ic_raindrop.svg" alt="" className="w-[3cqw] h-[3cqw]" />
+              <span className="text-[3cqw]">{day.rain}%</span>
             </div>
           </div>
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    screens: {
-      tiny: '0px',
-      sm: '400px',
-      md: '700px',
-      lg: '1000px',
-    },
     extend: {},
   },
   plugins: [require('@tailwindcss/container-queries')],
